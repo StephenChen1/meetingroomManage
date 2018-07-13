16cc2f85e533ec240ec7653f76372b825a662ffe
Reznik <724318695@qq.com>
2018-07-13 16:52:17 +0800

    完成会议室预约功能

--- src/main/java/web/RoomController.java
+++ src/main/java/web/RoomController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,13 +16,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import dao.ManagerDao;
 import dtoin.FreeTime;
 import dtoin.ModifyDevice;
+import dtoin.MyBookTime;
 import dtoin.SetFreeTime;
-import dtoout.AllBooked;
+import dtoin.UpdateBook;
 import dtoout.AllRoom;
 import entity.RoomDevice;
+import service.BookService;
+import service.JwtService;
 import service.ManagerService;
-
-
 
 @RequestMapping(value = "/room", method = RequestMethod.POST)
 @Controller
@@ -31,8 +33,10 @@ public class RoomController {
 	private ManagerService managerService;
 	@Autowired
 	private ManagerDao managerDao;
-	
-	
+	@Autowired
+	private BookService bookService;
+	@Autowired
+	private JwtService jwtService;
 
 	/**
 	 * 新增会议室
@@ -62,7 +66,6 @@ public class RoomController {
 	@ResponseBody
 	public boolean setFreeTime(@RequestBody SetFreeTime setFreeTime) {
 		String roomNumber = setFreeTime.getRoomNumber();
-		System.out.println("addRoomNumber:" + roomNumber);
 		List<FreeTime> freeTimeList = setFreeTime.getFreeTime();
 		return managerService.setFreeTime(roomNumber, freeTimeList);
 	}
@@ -79,108 +82,74 @@ public class RoomController {
 		String roomNumber = modifyDevice.getRoomNumber();
 		List<RoomDevice> roomDeviceList = modifyDevice.getRoomDevice();
 		return managerService.modifyDevice(roomNumber, roomDeviceList);
-	}        
-	
-	
+	}
+
 	/**
 	 * 获取所有会议室信息
+	 * 
 	 * @param 无
 	 * @return 所有会议室信息数据组
 	 */
 	@RequestMapping("/getAllRoom")
 	@ResponseBody
 	public List<AllRoom> getAllRooms(HttpServletRequest request) {
-		List<AllRoom> allRoom=managerService.getAllRooms();
+		List<AllRoom> allRoom = managerDao.getAllRooms();
 		return allRoom;
 	}
-	
+
 	/**
 	 * 根据id查询会议室获取该会议室信息
+	 * 
 	 * @param roomNumber
 	 * @return 该会议室信息数据
 	 */
 	@RequestMapping("/searchById")
 	@ResponseBody
-	public AllRoom searchRoomById(@RequestBody(required=false) Map<String,Object> map) {
+	public AllRoom searchRoomById(@RequestBody(required = false) Map<String, Object> map) {
 		String roomNumber = map.get("roomNumber").toString();
-		AllRoom allRoom=managerService.searchRoomById(roomNumber);
-		//注释代码用于测试
-		/*System.out.println("这里是Controller的roomNumber: " + roomNumber);
-		System.out.println("allRoom:"+allRoom.getCapability()+"   "+allRoom.getRoomNumber());
-		List<FreeTime> freetime=allRoom.getFreetime();
-		for(int i=0;i<freetime.size();i++){
-			System.out.println(freetime.get(i).getStartDate()+"  "+freetime.get(i).getEndDate()+"  "+freetime.get(i).getStartTime()+"  "+freetime.get(i).getEndTime());
-		}*/
+		AllRoom allRoom = managerDao.searchRoomById(roomNumber);
+		// 注释代码用于测试
+		/*
+		 * System.out.println("这里是Controller的roomNumber: " + roomNumber);
+		 * System.out.println("allRoom:"+allRoom.getCapability()+"   "+allRoom.
+		 * getRoomNumber()); List<FreeTime> freetime=allRoom.getFreetime(); for(int
+		 * i=0;i<freetime.size();i++){
+		 * System.out.println(freetime.get(i).getStartDate()+"  "+freetime.get(i).
+		 * getEndDate()+"  "+freetime.get(i).getStartTime()+"  "+freetime.get(i).
+		 * getEndTime()); }
+		 */
 		return allRoom;
 	}
-	
-	/**
-	 * 根据roomNumber删除会议室
-	 * @param roomNumber
-	 * @return boolean
-	 */
-	@RequestMapping("/deleteById")
+
+	@RequestMapping("/book")
 	@ResponseBody
-	public boolean deleteRoomById(@RequestBody(required=false) Map<String,Object> map) {
-		String roomNumber = map.get("roomNumber").toString();
-		System.out.println(roomNumber);
-		boolean flag=managerService.deleteRoomById(roomNumber);
-		return flag;
-	}
-	
-	/**
-	 * 查看全部员工所有预约记录
-	 * @param 无
-	 * @return List<AllBooked>
-	 */
-	@RequestMapping("/getAll")
-	@ResponseBody
-	public List<AllBooked> getAllBooked(HttpServletRequest request) {
-		List<AllBooked> allBooked=managerDao.getAllBooked();
-		return allBooked;
-	}
-	
-	/**
-	 * 查看对应员工的所有预约记录
-	 * @param roomNumber
-	 * @return List<AllBooked>
-	 */
-	@RequestMapping("/getPersonalBooked")
-	@ResponseBody
-	public List<AllBooked> getPersonalBooked(@RequestBody(required=false) Map<String,Object> map) {
-		String staffNumber = map.get("staffNumber").toString();
-		List<AllBooked> allBooked=managerDao.getPersonalBooked(staffNumber);
-		//注释代码用于测试
-		/*System.out.println("这里是Controller外的staffNumber: " + staffNumber);
-		System.out.println(allBooked.size());
-		for(int i=0;i<allBooked.size();i++){
-			AllBooked allRoom=allBooked.get(i);
-			System.out.println("这里是Controller里的staffNumber: " + staffNumber);
-			System.out.println("allRoom:"+allRoom.getCapability()+"   "+allRoom.getRoomNumber());
-			BookedTime freetime=allRoom.getBookedTime();
-			System.out.println(freetime.getStartDate()+"  "+freetime.getEndDate()+"  "+freetime.getStartTime()+"  "+freetime.getEndTime());
-			System.out.println("\n\n");
-		}*/
-		return allBooked;
-	}
-	
-	/**
-	 * 修改会议室容量
-	 * @param 会议室编号roomNumber
-	 * @param 新容量newCapability
-	 * @return boolean
-	 */
-	@RequestMapping(value = "/modifyCapability", method = RequestMethod.POST)
-	@ResponseBody
-	public boolean modifyPassword(@RequestBody(required=false) Map<String,Object> map) {
-		String roomNumber = map.get("roomNumber").toString();
-		int newCapability = Integer.valueOf(map.get("newCapability").toString());
-		//System.out.println("已进入modifyCapability方法");
-		//System.out.println(roomNumber+"   "+newCapability);
-		//boolean isOK = true;
-		boolean isOK = managerService.modifyCapability(roomNumber,newCapability);
-		//System.out.println("ISOK：" + isOK);
-		return isOK;
+	public List<MyBookTime> book(@RequestBody(required = true) UpdateBook updateBook,
+			@CookieValue(value = "token", required = false) String token) {
+		String canBookId = updateBook.getCanBookId();
+		MyBookTime bookedTime = updateBook.getMyBookTime();
+		// 插入一条booked数据，并获取其timeBookedId
+		String staffNumber = this.readCookie(token);
+		String roomNumber = bookService.getRoomNumber(canBookId);
+		int timeBookedId = bookService.addBookedAndReturnTBid(staffNumber, roomNumber);
+		// 插入time_booked数据
+		boolean isOK = bookService.addTimeBooked(timeBookedId, bookedTime.getStartDate(), bookedTime.getEndDate(),
+				bookedTime.getStartTime(), bookedTime.getEndTime());
+		//更新canBook表
+		List<MyBookTime> list = bookService.updateCanBook(canBookId, bookedTime);
+		return list;
 	}
 
+	/**
+	 * 私有方法，读取cookie中的token并解析为staffNumber
+	 * 
+	 * @param token
+	 * @return
+	 */
+	private String readCookie(@CookieValue(value = "token", required = false) String token) {
+		if (token == null) {
+			return null;
+		}
+		String staffNumber = jwtService.getStaffNum(token);
+		return staffNumber;
+	}
 }
